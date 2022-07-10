569d9101bf20a9b17a9d518842177e7a36e3614a
Olexander Bondarenko <olexanderbondarenko@olexanders-macbook-air.home>
2022-07-10 13:05:10 +0200

    add 0.0.3, releases

---
+++ prod/index.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector(".burger");
+const menuBurger = document.querySelector(".menu-burger");
+
+let burgerClickIndex = 0;
+
+function menuActive() {
+    if (burgerClickIndex == 0) {
+        menuBurger.classList.add("active");
+        burgerClickIndex++;
+    } else {
+        menuBurger.classList.remove("active");
+        burgerClickIndex--;
+    }
+}
+
+burger.addEventListener("click", menuActive);
+
+let animationOptions = {
+    root: document.querySelector(".why-us-container__img"),
+    threshold: 0.5,
+};
